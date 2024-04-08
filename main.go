a5442d46a366b25281c3dbfcbe9f32c57c4ae46d
Alex Diaz <alex@akdev.xyz>
2024-04-08 18:53:01 -0400

    fix: use correct attribute name and only consider crypt remotes

--- main.go
+++ main.go
@@ -4,11 +4,19 @@ import (
 	"fmt"
 	"github.com/rclone/rclone/fs/config"
 	"github.com/rclone/rclone/fs/config/obscure"
+	"log/slog"
 )
 
 func main() {
 	for _, section := range config.FileSections() {
-		pass := config.FileGet(section, "pass")
-		fmt.Println(section, obscure.MustReveal(pass))
+		remoteType := config.FileGet(section, "type")
+		if remoteType == "crypt" {
+			slog.Info("Found crypt remote", "remote", section)
+			password := config.FileGet(section, "password")
+			password2 := config.FileGet(section, "password2")
+			fmt.Printf("[%s]\n", section)
+			fmt.Println("password =", obscure.MustReveal(password))
+			fmt.Println("password2 =", obscure.MustReveal(password2))
+		}
 	}
 }
